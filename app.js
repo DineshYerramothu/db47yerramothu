47d020718ba707987e3937d9cbad56fa0ba66586
Dinesh Yerramothu <dyerramothu@gmail.com>
2021-11-18 22:39:41 -0600

    Files added

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+   Account.findOne({ username: username }, function (err, user) {
+    if (err) { return done(err); }
+    if (!user) {
+     return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+     return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+   });
+  }));
 var logger = require('morgan');
 
 const connectionString = process.env.MONGO_CON
@@ -34,6 +49,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -42,6 +64,13 @@ app.use('/echo', echoRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // We can seed the collection if needed on server start 
 async function recreateDB(){ 
